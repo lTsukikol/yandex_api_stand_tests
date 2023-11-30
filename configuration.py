ed49a4a3d077c1b6a89a1fc8e44edfc68357b47c
Tsukiko <tsukiko99.77@yandex.ru>
2023-12-01 02:35:26 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://4b0701f0-2c9b-4a6c-b3c9-3bb6a300bb25.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
